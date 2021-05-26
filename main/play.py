7eb4a32c95eeb0cfa6632d21a7f0e6d2b9d4d3da
jlpeppers1 <Monkey#12fuckgithubshit>
2021-05-25 23:55:20 -0500

    added topic 1 stuff

---
+++ main/play.py
@@ -0,0 +1,9 @@
+joe_string = "hello world"
+
+joe_string_capitalized = joe_string.capitalize()
+print("capitalized")
+print(joe_string_capitalized)
+
+my_list = ['apple', 'banana']
+#type a dot to see what methods you can use
+my_list.
