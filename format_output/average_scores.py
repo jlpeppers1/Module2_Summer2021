61edce36357d04a8fcb0dd62922af99133a186b7
jlpeppers1 <Monkey#12fuckgithubshit>
2021-05-25 23:39:27 -0500

    initial commit

---
+++ format_output/average_scores.py
@@ -0,0 +1,28 @@
+"""
+Program: average_scores.py
+Author: Joseph Peppers
+Last date modified: 05/25/2021
+TODO
+"""
+
+#get first name and set to a variable: first_name
+
+#get last name and set to a variable: last_name
+
+#get age and set to a variable
+#this one was given to us in Topic 2
+age = input("Enter your age:")
+
+#get user score1 out of 100
+
+#get user score2 out of 100
+
+#get user score3 out of 100
+
+#TODO: Set up constants.py file like we did on Mod1, and pull that value in
+#take the 3 scores and find the average, stored as a variable
+     #for an average, you add up all values and divide by number of values
+
+
+#TODO format variables and make output look something like:
+# Rodriguez, Linda age: 21 average grade: 92.50
